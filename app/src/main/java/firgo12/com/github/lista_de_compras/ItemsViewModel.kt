b1bd0a7c10cc1287d857b890a38e3935eeb1c152
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-02 21:54:50 -0300

    first commit

---
+++ app/src/main/java/firgo12/com/github/lista_de_compras/ItemsViewModel.kt
@@ -0,0 +1,33 @@
+package firgo12.com.github.lista_de_compras
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+/**
+ * O objetivo dessa ViewModel será abstrair comportamentos
+ * que, até então, estavam sendo feitos na Activity e no Adapter
+ * em nosso projeto.
+ *
+ *
+ */
+class ItemsViewModel : ViewModel() {
+
+    private var items = mutableListOf<ItemModel>()
+    val itemsLiveData = MutableLiveData<List<ItemModel>>()
+
+    fun addItem(name: String) {
+        val item = ItemModel(
+            id = 0,
+            name = name,
+            onRemove = ::removeItem
+        )
+
+        items.add(item)
+        itemsLiveData.value = items
+    }
+
+    private fun removeItem(item: ItemModel) {
+        items.remove(item)
+        itemsLiveData.value = items
+    }
+}
